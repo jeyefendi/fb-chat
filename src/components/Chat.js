fc13fb79f88eae06361dd08e0ddd390745a74421
Jeyhun Farajov <97691925+jeyefendi@users.noreply.github.com>
2022-11-17 00:26:28 +0300

    Cloud Firestore messages

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { useContext, useState } from "react";
+import { Context } from "../index";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Button, Grid, TextField } from "@mui/material";
+import { Container } from "@mui/system";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import Loader from "./Loader";
+import firebase from 'firebase/compat/app';
 
 const Chat = () => {
-    return (
-        <div>
-            Chat
-        </div>
-    );
+  const { auth, firestore } = useContext(Context)
+  const [user] = useAuthState(auth)
+  const [value, setValue] = useState('')
+  const [messages, loading] = useCollectionData(
+    firestore.collection('messages').orderBy('createdAt')
+  )
+
+  const sendMessage = async () => {
+    firestore.collection('messages').add({
+        uid: user.uid,
+        displayName: user.displayName,
+        photoURL: user.photoURL,
+        text: value,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    setValue('')
+  }
+
+  if (loading) {
+    return <Loader/>
+  }
+
+  return (
+    <Container>
+      <Grid
+        container
+        justifyContent={"center"}
+        style={{ height: window.innerHeight - 50, paddingTop: 20 }}
+      >
+        <div
+          style={{
+            width: "80%",
+            height: "70vh",
+            border: "1px solid gray",
+            overflow: "auto",
+          }}
+        ></div>
+        <Grid
+          container
+          direction={"column"}
+          alignItems={"flex-end"}
+          style={{ width: "80%", gap: "0.5em" }}
+        >
+          <TextField
+            fullWidth
+            rowsMax={2}
+            variant={"outlined"}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <Button onClick={sendMessage} variant={"outlined"}>Отправить</Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Chat;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
 import { AppBar, Button, Grid, Toolbar } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { NavLink } from "react-router-dom";
+import { Context } from "..";
 import { LOGIN_ROUTE } from "../utils/consts";
 
 const Navbar = () => {
-  const user = false;
+  const {auth} = useContext(Context)
+  const [user] = useAuthState(auth)
+
   return (
     <AppBar color={"secondary"} position="static">
       <Toolbar variant="dense">
         <Grid container justifyContent="flex-end">
           {user ? (
-            <Button variant={"outlined"}>Выйти</Button>
+            <Button onClick={() => auth.signOut()} variant={"outlined"}>Выйти</Button>
           ) : (
             <NavLink to={LOGIN_ROUTE}>
               <Button variant={"outlined"}>Логин</Button>
